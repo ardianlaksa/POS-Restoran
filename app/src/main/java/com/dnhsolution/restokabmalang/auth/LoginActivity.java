c90dc3c1d06dc9c9199af8f7188c88b4d30b73b8
UniqueCode <hambasahaya26@gmail.com>
2019-06-15 09:10:06 +0700

    add drawable, background login 2

---
+++ app/src/main/java/com/dnhsolution/restokabmalang/auth/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.dnhsolution.restokabmalang.auth;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.dnhsolution.restokabmalang.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
