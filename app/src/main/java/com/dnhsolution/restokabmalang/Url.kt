c59a02fd9cfc5f70f6de56c863bd2ad3f956466d
UniqueCode <hambasahaya26@gmail.com>
2019-06-17 16:53:12 +0700

    update login

--- app/src/main/java/com/dnhsolution/restokabmalang/Url.kt
+++ app/src/main/java/com/dnhsolution/restokabmalang/Url.kt
@@ -2,4 +2,9 @@ package com.dnhsolution.restokabmalang
 
 object Url {
     const val serverPdrd = "http://devdeenha.ddns.net:8080/pdrd/"
+    const val serverPos = "http://devdeenha.ddns.net:8080/android/pos/androidjson/"
+    const val SESSION_USERNAME = "username"
+    const val SESSION_PASSWORD = "password"
+    const val SESSION_STS_LOGIN = "status"
+    const val SESSION_NAME = "pos"
 }
